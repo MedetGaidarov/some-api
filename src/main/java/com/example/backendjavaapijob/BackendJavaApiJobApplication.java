d8ba72d57d828f8b414de6d82bb3e07f5bf8b896
Medet Gaidarov <mgaiidarov@gmail.com>
2022-11-16 13:19:38 +0500

    init commit

---
+++ src/main/java/com/example/backendjavaapijob/BackendJavaApiJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendjavaapijob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApiJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaApiJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendjavaapijob/BackendJavaApiJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendjavaapijob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApiJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
