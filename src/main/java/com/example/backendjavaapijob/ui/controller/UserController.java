8cc02ffc8ffda8fd8d79aef0baa800fb44058d7a
Medet Gaidarov <mgaiidarov@gmail.com>
2022-11-16 23:58:52 +0500

    User Controller changed

--- src/main/java/com/example/backendjavaapijob/ui/controller/UserController.java
+++ src/main/java/com/example/backendjavaapijob/ui/controller/UserController.java
@@ -3,13 +3,16 @@ package com.example.backendjavaapijob.ui.controller;
 
 import com.example.backendjavaapijob.domain.user.model.User;
 import com.example.backendjavaapijob.domain.user.service.UserService;
+import com.example.backendjavaapijob.ui.dto.DefaultResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("api/user")
 public class UserController {
 
     @Autowired
@@ -17,9 +20,12 @@ public class UserController {
 
 
     @GetMapping("{id}")
-    public User getUser(Long id)
-    {
-        return userService.getUserById(id);
+    public ResponseEntity<Object> getUser(@RequestParam Long id) {
+        try {
+            return ResponseEntity.ok(new DefaultResponseDto("SUCCESS", "User successfully found", userService.getUserById(id)));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new DefaultResponseDto("FAULT", e.getMessage()));
+        }
     }
 
 }
